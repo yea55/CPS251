1f9d49d8d47371da711047165b8f6f77672b1096
Ian Wait <iwait@wccnet.edu>
2022-02-12 17:23:59 -0500

    [d]

--- autopush.bat
+++ autopush.bat
@@ -1,8 +1,8 @@
 @echo off
-set /p desc="Enter commit description: "
-IF NOT [%desc] == [] goto skip
-set desc=autopush.bat commit
+set /p d="Enter commit description: "
+IF NOT [%d] == [] goto skip
+set d=autopush.bat commit
 :skip
 git add .
-git commit -m "[%desc]"
+git commit -m "[%d]"
 git push origin master
