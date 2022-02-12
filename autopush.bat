0497ba1be9a998a107bdeb40b82adbc51bd0a9bc
Ian Wait <iwait@wccnet.edu>
2022-02-12 17:31:57 -0500

    autopush.bat commit

--- autopush.bat
+++ autopush.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p d="Enter commit description: "
-IF NOT [%d] == [""] goto skip
-set d=autopush.bat commit
+IF NOT [%d%] == [] goto skip
+set d="autopush.bat commit"
 :skip
 git add .
 git commit -m %d%
